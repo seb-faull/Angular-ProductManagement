ea911b2042103e7f35b11145e40f558fdce07572
bxxstian <seb_faull@hotmail.co.uk>
2019-04-29 16:06:55 +0100

    Initial commit & install bootstrap and font-awesome

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // Selector used as a directive in the HTML (a custom element)
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+    `
+})
+export class AppComponent {
+// The 'export' keyword ensures other parts of the app can use this class
+  pageTitle: string = 'Acme Product Management';
+}
