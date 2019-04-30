0b4feb6d0f7a6b9b1bb6df98e000eab90d7ee7b1
bxxstian <seb_faull@hotmail.co.uk>
2019-04-30 10:49:09 +0100

    Handle input with two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  // Our directives, components and pipes are declared in the declarations array
   declarations: [
     AppComponent,
     ProductListComponent
   ],
+  // Directives, components and pipes used from other sources such as Angular or
+  // third parties are delcared in the imports array
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
