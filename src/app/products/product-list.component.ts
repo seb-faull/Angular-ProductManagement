2dea4c5422672c926605115890d7db679d09a037
bxxstian <seb_faull@hotmail.co.uk>
2019-04-30 10:19:03 +0100

    Add css property binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
   products: any[] = [
     {
       "productId": 2,
@@ -15,7 +17,7 @@ export class ProductListComponent {
       "description": "15 gallon capacity rolling garden cart",
       "price": 32.99,
       "starRating": 4.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+      "imageUrl": "https://longfordpc.com/images/carts-clipart-garden-cart-8.png"
     },
     {
       "productId": 5,
@@ -25,7 +27,7 @@ export class ProductListComponent {
       "description": "Curved claw steel hammer",
       "price": 8.9,
       "starRating": 4.8,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }
   ];
 }
