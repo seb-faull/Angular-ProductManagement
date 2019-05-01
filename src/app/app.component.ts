f87b37e9b3921bc79a657036b209d65c64c057c5
bxxstian <seb_faull@hotmail.co.uk>
2019-05-01 16:07:54 +0100

    Tie routes to actions and place in views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { Component } from '@angular/core';
   // Selector used as a directive in the HTML (a custom element)
   selector: 'pm-root',
   template: `
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+        <a class='navbar-brand'>{{ pageTitle }}</a>
+        <ul class='nav nav-pills'>
+          <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+          <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+        </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
     </div>
-    `
+  `
 })
 export class AppComponent {
 // The 'export' keyword ensures other parts of the app can use this class
