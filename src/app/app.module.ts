7a9d272e035512bc977a8fe4c021e23bc072671f
bxxstian <seb_faull@hotmail.co.uk>
2019-05-01 11:33:55 +0100

    Retreive data using http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -19,7 +20,8 @@ import { StarComponent } from './shared/star.component';
   // third parties are delcared in the imports array
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
